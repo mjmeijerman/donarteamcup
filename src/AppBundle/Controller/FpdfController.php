c32052a80a580c57fe43251754bed008e0078705
Mark Meijerman <mark@future500.nl>
2018-09-24 16:55:51 +0200

    add fonts

--- src/AppBundle/Controller/FpdfController.php
+++ src/AppBundle/Controller/FpdfController.php
@@ -108,8 +108,8 @@ class FpdfController
             $this->fontpath = FPDF_FONTPATH;
             if (substr($this->fontpath, -1) != '/' && substr($this->fontpath, -1) != '\\')
                 $this->fontpath .= '/';
-        } elseif (is_dir(dirname(__FILE__) . '/font'))
-            $this->fontpath = dirname(__FILE__) . '/font/';
+        } elseif (is_dir(dirname(__FILE__) . '/../font'))
+            $this->fontpath = dirname(__FILE__) . '/../font/';
         else
             $this->fontpath = '';
         // Core fonts
